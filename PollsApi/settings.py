ed3402761ddfc98412b595dc335b9c637c2bdbe5
sydneyIdundun <sydneyidundun@Sydneys-MacBook-Air.local>
2022-09-07 04:17:00 +0400

    turned on debug mode

--- PollsApi/settings.py
+++ PollsApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x_)1$$a_=#$^j+8*-^9re9+m#jmek$z3i*6okh89=)d#k@(9s#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://pollsapi-production.up.railway.app']
